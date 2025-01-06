a3dea3016642504f5b8c162cf9cd05b333ba7e3e
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-01-06 14:12:38 -0500

    Handle multiple sequential request from a client and fix messageSize bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ import java.net.Socket;
 import java.nio.ByteBuffer;
 
 public class Main {
+  static short minSupportedAPIVersion = 0;
+  static short maxSupportedAPIVersion = 4;
+
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible
     // when running tests.
@@ -16,8 +19,7 @@ public class Main {
     OutputStream out = null;
     InputStream in = null;
     int port = 9092;
-    short minSupportedAPIVersion = 0;
-    short maxSupportedAPIVersion = 4; 
+
     try {
       serverSocket = new ServerSocket(port);
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
@@ -25,39 +27,67 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
-      in = clientSocket.getInputStream();
-      byte[] messageSizeBytes = new byte[4];
-      in.read(messageSizeBytes);
-      int messageSize = ByteBuffer.wrap(messageSizeBytes).getInt();
-      byte[] requestBytes = new byte[messageSize];
-      in.read(requestBytes);
-      ByteBuffer buffer = ByteBuffer.wrap(requestBytes);
-      short apiKey = readInt16(buffer);
-      short apiVersion = readInt16(buffer);
-      int correlationId = readInt32(buffer);
-    
+      handleClient(clientSocket);
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
 
-      int message_size = 4 + 2 + 1 + 2 + 2 + 2 + 1 + 4 + 1;
-
-      ByteBuffer outputBuffer = ByteBuffer.allocate(4 + messageSize);
-      outputBuffer.putInt(message_size);
-      outputBuffer.putInt(correlationId);
-
-      if(apiVersion < minSupportedAPIVersion || apiVersion > maxSupportedAPIVersion) {
-        outputBuffer.putShort((short)35); // Error code for unsupported version
-      }else{
-        outputBuffer.putShort((short)0); // Error code for no error
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
       }
-      outputBuffer.put((byte)2); // number of api keys supported, since api keys is compact array we use N + 1 to represent N elements
-      outputBuffer.putShort((short)apiKey); 
-      outputBuffer.putShort(minSupportedAPIVersion); // min Api version
-      outputBuffer.putShort(maxSupportedAPIVersion); // max Api version
-      outputBuffer.put((byte)0); // api key level tag buffer representing null array in compact array format
-      outputBuffer.putInt(0); // throttle time 
-      outputBuffer.put((byte)0); // tag buffer of size 0 represents null array in compact array format 
+    }
+  }
+
+  private static void handleClient(Socket clientSocket) {
+    InputStream in = null;
+    OutputStream out = null;
+    try {
+      in = clientSocket.getInputStream();
       out = clientSocket.getOutputStream();
-      out.write(outputBuffer.array());
-      out.flush();
+      while (true) {
+        byte[] messageSizeBytes = new byte[4];
+        in.read(messageSizeBytes);
+        int messageSize = ByteBuffer.wrap(messageSizeBytes).getInt();
+        if(messageSize == -1){
+          System.out.println("Connection closed by client");
+          break;
+        }
+        System.out.println("Message size: " + messageSize);
+        byte[] requestBytes = new byte[messageSize];
+        in.read(requestBytes);
+        ByteBuffer buffer = ByteBuffer.wrap(requestBytes);
+        short apiKey = readInt16(buffer);
+        short apiVersion = readInt16(buffer);
+        int correlationId = readInt32(buffer);
+
+        messageSize = 4 + 2 + 1 + 2 + 2 + 2 + 1 + 4 + 1;
+
+        ByteBuffer outputBuffer = ByteBuffer.allocate(4 + messageSize);
+        outputBuffer.putInt(messageSize);
+        outputBuffer.putInt(correlationId);
+
+        if (apiVersion < minSupportedAPIVersion || apiVersion > maxSupportedAPIVersion) {
+          outputBuffer.putShort((short) 35); // Error code for unsupported version
+        } else {
+          outputBuffer.putShort((short) 0); // Error code for no error
+        }
+        outputBuffer.put((byte) 2); // number of api keys supported, since api keys is compact array we use N + 1 to
+                                    // represent N elements
+        outputBuffer.putShort((short) apiKey);
+        outputBuffer.putShort(minSupportedAPIVersion); // min Api version
+        outputBuffer.putShort(maxSupportedAPIVersion); // max Api version
+        outputBuffer.put((byte) 0); // api key level tag buffer representing null array in compact array format
+        outputBuffer.putInt(0); // throttle time
+        outputBuffer.put((byte) 0); // tag buffer of size 0 represents null array in compact array format
+
+        out.write(outputBuffer.array());
+        out.flush();
+
+      }
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
